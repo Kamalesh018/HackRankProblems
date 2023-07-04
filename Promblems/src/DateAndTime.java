2047c5fcc55c24b5590d3b5def200fcc31c150eb
Kamalesh018 <kamaleshkb77@gmail.com>
2023-07-04 21:53:27 +0530

    edit new solution

--- Promblems/src/DateAndTime.java
+++ Promblems/src/DateAndTime.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -6,13 +7,11 @@ public class DateAndTime {
 
     }
     public static String findDay(int month, int day, int year) {
-        Calendar cal =  Calendar.getInstance();
-        month = cal.get(Calendar.MONTH);
-        day = cal.get(Calendar.DAY_OF_MONTH);
-        year = cal.get(Calendar.YEAR);
 
-        String[] strArr = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-        return (strArr.toString().toUpperCase());
+        Calendar cal = Calendar.getInstance();
+        cal.set(year,month-1,day);
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        return sdf.format(cal.getTime()).toUpperCase();
 
     }
 }
